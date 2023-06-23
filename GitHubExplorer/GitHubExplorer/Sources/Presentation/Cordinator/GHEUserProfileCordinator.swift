12edf2a994cbedb39e5c9281bc1f195aae1a509a
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-23 18:08:51 -0300

    created GHEUserProfileCordinator and new refernces

---
+++ GitHubExplorer/GitHubExplorer/Sources/Presentation/Cordinator/GHEUserProfileCordinator.swift
@@ -0,0 +1,31 @@
+//
+//  GHEUserProfileCordinator.swift
+//  GitHubExplorer
+//
+//  Created by Danilo Carlos Ribeiro on 23/06/23.
+//  Copyright © 2023 GitHubExplorer. All rights reserved.
+//
+
+import UIKit
+
+public class GHEUserProfileCordinator {
+    
+    // MARK: - Properties
+    private var navigationController: UINavigationController?
+
+    // MARK: - Initialize
+    public init(navigationController: UINavigationController?) {
+        self.navigationController = navigationController
+    }
+    
+    // MARK: - Public Methods
+    func start(model: GHEResponse?) {
+        let viewModel = GHEUserProfileViewModel(model: model)
+        guard let viewController = GHEUserProfileViewController.instantiate(viewModel: viewModel) else { return }
+        viewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    // MARK: - Private Methods
+    private func showNextFlow(model: GHEResponse?) {}
+}
