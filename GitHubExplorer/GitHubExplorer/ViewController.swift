32a8b0c4350527c31596a640ba2abd0f4cb28dc1
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-22 21:08:16 -0300

    add first project configuration

---
+++ GitHubExplorer/GitHubExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubExplorer
+//
+//  Created by Danilo Carlos Ribeiro on 22/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
