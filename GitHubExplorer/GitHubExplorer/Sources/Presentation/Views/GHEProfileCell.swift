382d39c7232e1783332b286c750b426820f4a30b
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-23 16:40:50 -0300

    code improvements

--- GitHubExplorer/GitHubExplorer/Sources/Presentation/Views/GHEProfileCell.swift
+++ GitHubExplorer/GitHubExplorer/Sources/Presentation/Views/GHEProfileCell.swift
@@ -12,12 +12,17 @@ class GHEProfileCell: UITableViewCell {
     
     // MARK: - Constants
     private enum Constants {
-        static let imageNameDefault: String = "image_default"
         static let imageViewSize: CGFloat = 70
         static let avatarViewBoderWidth: CGFloat = 2
+        static let moreInfoText = "Mais informações"
+        static let textDownloadError = "Erro ao fazer o download da imagem:"
+        static let textNotFoundData = "Dados da imagem não encontrados."
+        static let texImageCorrupted = "Não foi possível criar a imagem a partir dos dados recebidos."
     }
     
     // MARK: - UI
+    private let contentAvatarView = UIView()
+    
     private lazy var avatarView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
@@ -34,33 +39,84 @@ class GHEProfileCell: UITableViewCell {
         return label
     }()
     
+    private lazy var moreInfoLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.text = Constants.moreInfoText
+        label.textColor = .blue
+        return label
+    }()
+    
+    var imageUrl: String?
+    
     // MARK: - Setup
     open func setup(title: String?, avatarUrl: String?) {
         titleLabel.text = title
+        imageUrl = avatarUrl
         setupUI()
     }
     
     private func setupUI() {
         selectionStyle = .none
+        getAvatarUrl()
+        setupContentAvatarView()
         setupAvatarView()
         setupTitleLabel()
+        setupMoreInfoLabel()
+    }
+    
+    private func setupContentAvatarView() {
+        contentView.addSubview(contentAvatarView)
+        contentAvatarView.myAnchor(
+            top: (contentView.topAnchor, 24),
+            leading: (contentView.leadingAnchor, 24),
+            bottom: (contentView.bottomAnchor, 24),
+            width: (128),
+            height: (128))
+    }
+    
+    private func getAvatarUrl() {
+        if let imageUrlString = imageUrl, let url = URL(string: imageUrlString) {
+            let task = URLSession.shared.dataTask(with: url) { data, _, error in
+                if let error = error {
+                    print("\(Constants.textDownloadError) \(error.localizedDescription)")
+                    return
+                }
+                guard let imageData = data else {
+                    print(Constants.textNotFoundData)
+                    return
+                }
+                if let image = UIImage(data: imageData) {
+                    DispatchQueue.main.async {
+                        self.avatarView.image = image
+                    }
+                } else {
+                    print(Constants.texImageCorrupted)
+                }
+            }
+            task.resume()
+        }
     }
     
     private func setupAvatarView() {
-        contentView.addSubview(avatarView)
-        avatarView.myAnchor(top: (contentView.topAnchor, 24),
-                            leading: (contentView.leadingAnchor, 24),
-                            bottom: (contentView.bottomAnchor, 24)
-        )
+        contentAvatarView.addSubview(avatarView)
+        avatarView.myFillSuperview()
     }
     
     private func setupTitleLabel() {
         contentView.addSubview(titleLabel)
-        titleLabel.myAnchor(centerY: (avatarView.centerYAnchor, .zero),
-                            top: (contentView.topAnchor, 24),
-                            leading: (avatarView.trailingAnchor, 24),
-                            trailing: (contentView.trailingAnchor, 24),
-                            bottom: (contentView.bottomAnchor, 24)
+        titleLabel.myAnchor(
+            top: (contentView.topAnchor, 36),
+            trailing: (contentView.trailingAnchor, 24)
+        )
+    }
+    
+    private func setupMoreInfoLabel() {
+        contentView.addSubview(moreInfoLabel)
+        moreInfoLabel.myAnchor(
+            top: (titleLabel.bottomAnchor, 4),
+            trailing: (contentView.trailingAnchor, 24),
+            bottom: (contentView.bottomAnchor, 24)
         )
     }
 }
