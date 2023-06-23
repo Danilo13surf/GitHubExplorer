4b474d9e4782733825ac2af33ba49e78817edc16
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-23 00:31:09 -0300

    created GHEResponse

---
+++ GitHubExplorer/GitHubExplorer/Sources/Data/Model/Response/GHEResponse.swift
@@ -0,0 +1,31 @@
+//
+//  GHEResponse.swift
+//  GitHubExplorer
+//
+//  Created by Danilo Carlos Ribeiro on 23/06/23.
+//  Copyright © 2023 GitHubExplorer. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - GHEResponse
+struct GHEResponse {
+    let login: String?
+    let id: Int?
+    let nodeID: String?
+    let avatarURL: String?
+    let gravatarID: String?
+    let url: String?
+    let htmlURL: String?
+    let followersURL: String?
+    let followingURL: String?
+    let gistsURL: String?
+    let starredURL: String?
+    let subscriptionsURL: String?
+    let organizationsURL: String?
+    let reposURL: String?
+    let eventsURL: String?
+    let receivedEventsURL: String?
+    let type: String?
+    let siteAdmin: Bool?
+}
