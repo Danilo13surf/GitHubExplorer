0fcc2fd305c8d458fdde2021ac8095eab0669704
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-23 08:20:05 -0300

    created GHEConstants

---
+++ GitHubExplorer/GitHubExplorer/Sources/Infrastructure/Constants/GHEConstants.swift
@@ -0,0 +1,17 @@
+//
+//  GHEConstants.swift
+//  GitHubExplorer
+//
+//  Created by Danilo Carlos Ribeiro on 23/06/23.
+//  Copyright © 2023 GitHubExplorer. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - GHEConstants
+enum GHEConstants {
+    // MARK: - PathApi
+    class GHEPathApi {
+        let users = "https://api.github.com/users"
+    }
+}
