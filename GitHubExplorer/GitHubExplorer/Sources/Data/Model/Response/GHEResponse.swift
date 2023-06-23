5b01c31c7c7bee53f665cb454c727ce74c82a89b
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-23 16:01:05 -0300

    code review in GHEResponse

--- GitHubExplorer/GitHubExplorer/Sources/Data/Model/Response/GHEResponse.swift
+++ GitHubExplorer/GitHubExplorer/Sources/Data/Model/Response/GHEResponse.swift
@@ -12,20 +12,20 @@ import Foundation
 struct GHEResponse: Codable {
     let login: String?
     let id: Int?
-    let nodeID: String?
-    let avatarURL: String?
-    let gravatarID: String?
+    let node_id: String?
+    let avatar_url: String?
+    let gravatar_id: String?
     let url: String?
-    let htmlURL: String?
-    let followersURL: String?
-    let followingURL: String?
-    let gistsURL: String?
-    let starredURL: String?
-    let subscriptionsURL: String?
-    let organizationsURL: String?
-    let reposURL: String?
-    let eventsURL: String?
-    let receivedEventsURL: String?
+    let html_url: String?
+    let followers_url: String?
+    let following_url: String?
+    let gists_url: String?
+    let starred_url: String?
+    let subscriptions_url: String?
+    let organizations_url: String?
+    let repos_url: String?
+    let events_url: String?
+    let received_events_url: String?
     let type: String?
-    let siteAdmin: Bool?
+    let site_admin: Bool?
 }
