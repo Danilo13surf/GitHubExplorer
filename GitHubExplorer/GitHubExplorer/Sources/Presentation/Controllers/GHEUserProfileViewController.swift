15d317b77095d19d2e4c6875fb6b0b3f3738e939
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-23 18:14:10 -0300

    created GHEUserProfileViewController.swift

---
+++ GitHubExplorer/GitHubExplorer/Sources/Presentation/Controllers/GHEUserProfileViewController.swift
@@ -0,0 +1,106 @@
+//
+//  GHEUserProfileViewController.swift
+//  GitHubExplorer
+//
+//  Created by Danilo Carlos Ribeiro on 23/06/23.
+//  Copyright © 2023 GitHubExplorer. All rights reserved.
+//
+
+import UIKit
+
+class GHEUserProfileViewController: BaseViewController {
+    
+    // MARK: - Constants
+    private enum Constants {
+        static let userTitle = "Usuario:"
+    }
+    
+    // MARK: - Properties
+    private var viewModel: GHEUserProfileViewModelProtocol?
+    
+    // MARK: - UI
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.registerCell(GHEProfileCell.self)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.bounces = false
+        tableView.showsVerticalScrollIndicator = false
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.separatorInset = .zero
+        tableView.layoutMargins = .zero
+        return tableView
+    }()
+    
+    // MARK: - Instantiate
+    static func instantiate(viewModel: GHEUserProfileViewModelProtocol) -> GHEUserProfileViewController? {
+        let viewController = GHEUserProfileViewController()
+        viewController.viewModel = viewModel
+        return viewController
+    }
+    
+    // MARK: - Override Methods
+    override func setupUI() {
+        super.setupUI()
+        titleText = "\(Constants.userTitle) \(getUserName())"
+        setupTableView()
+    }
+    
+    // MARK: - Private Methods
+    private func getUserName() -> String {
+        if let login = viewModel?.model?.login {
+            return login
+        }
+        return String()
+    }
+    
+    private func setupTableView() {
+        contentView.addSubview(tableView)
+        tableView.myAnchor(top: (contentView.topAnchor, 38),
+                           leading: (contentView.leadingAnchor, 24),
+                           trailing: (contentView.trailingAnchor, 24),
+                           bottom: (contentView.bottomAnchor, 24)
+        )
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension GHEUserProfileViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        guard let viewModel = viewModel else { return .zero }
+        return viewModel.numberOfSections()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let viewModel = viewModel else { return .zero }
+        if section == .zero {
+            return 1
+        }
+        return viewModel.listRepository?.count ?? .zero
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(GHEProfileCell.self, for: indexPath)
+        if indexPath.section == .zero && viewModel?.isDisplayingRepository.value == false {
+            cell.setup(title: viewModel?.model?.login, avatarUrl: viewModel?.model?.avatar_url, style: .completeInfo)
+            return cell
+        }
+        let listRepository = viewModel?.listRepository?[indexPath.row]
+        cell.setup(title: listRepository?.login, avatarUrl: listRepository?.avatar_url, style: .completeInfo)
+        return cell
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension GHEUserProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        DispatchQueue.main.async { [self] in
+            viewModel?.isDisplayingRepository.value = true
+            tableView.reloadData()
+        }
+    }
+}
