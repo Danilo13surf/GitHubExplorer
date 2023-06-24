ad8fc4cfae6fd6915ef94838aae061e4d7c4fb6e
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-24 07:29:07 -0300

    created GHEUserProfileManager

--- GitHubExplorer/GitHubExplorer/Sources/Features/UserProfile/Data/Manager/GHEUserProfileManager.swift
+++ GitHubExplorer/GitHubExplorer/Sources/Features/UserProfile/Data/Manager/GHEUserProfileManager.swift
@@ -1,5 +1,5 @@
 //
-//  GHE.swift
+//  GHEUserProfileManager.swift
 //  GitHubExplorer
 //
 //  Created by Danilo Carlos Ribeiro on 24/06/23.
@@ -17,4 +17,22 @@ protocol GHEUserProfileManagerProtocol {
 
 class GHEUserProfileManager: BaseManager, GHEUserProfileManagerProtocol {
     
+    // MARK: - Properties
+    var provider: ApiProviderProtocol?
+    var business: GHEUserProfileBussinesProtocol?
+    
+    // MARK: - Initialize
+    
+    required init(provider: ApiProviderProtocol? = nil, business: GHEUserProfileBussinesProtocol = GHEUserProfileBussines()) {
+        self.provider = provider
+        self.business = business
+        super.init()
+    }
+    
+    // MARK: - Public Methods
+    func getRepositorys(completion: @escaping GHEGetRepositorysCompletion) {
+        cancelAllOperations()
+        let operation = GHEListRepositorysOperation(provider: provider, business: business, completion: completion)
+        addOperation(operation)
+    }
 }
