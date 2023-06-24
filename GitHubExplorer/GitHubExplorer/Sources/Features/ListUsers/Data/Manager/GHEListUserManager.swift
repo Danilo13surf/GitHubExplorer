64c30058a9fadd6c3e81e61ca5f5de5169daf6cd
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-24 07:29:35 -0300

    created GHEListUserManager

--- GitHubExplorer/GitHubExplorer/Sources/Features/ListUsers/Data/Manager/GHEListUserManager.swift
+++ GitHubExplorer/GitHubExplorer/Sources/Features/ListUsers/Data/Manager/GHEListUserManager.swift
@@ -16,4 +16,24 @@ protocol GHEListUserManagerProtocol {
 }
 
 class GHEListUserManager: BaseManager, GHEListUserManagerProtocol {
+    
+    // MARK: - Properties
+    var provider: ApiProviderProtocol?
+    var business: GHEListUsersBusinessProtocol?
+    
+    // MARK: - Initialize
+    
+    required init(provider: ApiProviderProtocol? = nil, business: GHEListUsersBusinessProtocol = GHEListUsersBusiness()) {
+        self.provider = provider
+        self.business = business
+        super.init()
+    }
+    
+    // MARK: - Public Methods
+    
+    func fetchUssers(completion: @escaping GHEGetUssersCompletion) {
+        cancelAllOperations()
+        let operation = GHEListUsersOperation(provider: provider, business: business, completion: completion)
+        addOperation(operation)
+    }
 }
