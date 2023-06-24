6b3de1e96e7e4eebecc8315f41a2dfa5fab4e8d1
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-24 08:43:23 -0300

    add new helper for date formater

---
+++ GitHubExplorer/GitHubExplorer/SupportFiles/CoreComponents/Helpers/Extensions/DateFormater.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormater.swift
+//  GitHubExplorer
+//
+//  Created by Danilo Carlos Ribeiro on 24/06/23.
+//  Copyright © 2023 GitHubExplorer. All rights reserved.
+//
+
+import UIKit
+
+extension DateFormatter {
+    static let iso8601Full: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return formatter
+    }()
+}
