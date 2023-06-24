449f3da20fb390917f395ee8d29b992c4cf5f2d2
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-24 07:36:13 -0300

    created GHEListRepositorysOperation

--- GitHubExplorer/GitHubExplorer/Sources/Features/UserProfile/Data/Operation/GHEListRepositorysOperation.swift
+++ GitHubExplorer/GitHubExplorer/Sources/Features/UserProfile/Data/Operation/GHEListRepositorysOperation.swift
@@ -8,6 +8,34 @@
 
 import CoreSwift
 import Foundation
+
 class GHEListRepositorysOperation: NetworkingOperation {
     
+    // MARK: - Properties
+    private let business: GHEUserProfileBussinesProtocol?
+    private let completion: GHEGetRepositorysCompletion
+    
+    // MARK: Initialize
+    init(provider: ApiProviderProtocol? = nil, business: GHEUserProfileBussinesProtocol?, completion: @escaping GHEGetRepositorysCompletion) {
+        self.business = business
+        self.completion = completion
+        super.init(provider: provider)
+    }
+    
+    // MARK: - Override Methods
+    override func main() {
+        super.main()
+        provider?.baseRequest(requestData: buildRequestData()) { [weak self] result in
+            guard let self = self else { return }
+            self.business?.handleRepositorysGet(result: result, completion: self.completion)
+            self.finish()
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func buildRequestData() -> NetworkingRequestData {
+        return NetworkingRequestData(url: GHEConstants.GHEPathApi().users,
+                                     httpMethod: .get,
+                                     isAuthenticated: true)
+    }
 }
