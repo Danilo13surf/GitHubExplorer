06094b07acba08837b28b452e800977a82f4055e
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-25 19:02:36 -0300

    add new parameters in tests

--- GitHubExplorer/GitHubExplorerTests/Features/ListUsers/Domain/ViewModel/GHEListUsersViewModelTests.swift
+++ GitHubExplorer/GitHubExplorerTests/Features/ListUsers/Domain/ViewModel/GHEListUsersViewModelTests.swift
@@ -57,8 +57,8 @@ class GHEListUsersViewModelTests: XCTestCase {
     // MARK: - Private Methods for set mock response
     private func setMockGHEResponse() -> [GHEResponse] {
         return [
-            GHEResponse(login: "user1", id: 1, node_id: "node1", avatar_url: "avatar1", gravatar_id: "gravatar1", url: "url1", html_url: "html1", followers_url: "followers1", following_url: "following1", gists_url: "gists1", starred_url: "starred1", subscriptions_url: "subscriptions1", organizations_url: "organizations1", repos_url: "repos1", events_url: "events1", received_events_url: "received_events1", type: "type1", site_admin: false),
-            GHEResponse(login: "user2", id: 2, node_id: "node2", avatar_url: "avatar2", gravatar_id: "gravatar2", url: "url2", html_url: "html2", followers_url: "followers2", following_url: "following2", gists_url: "gists2", starred_url: "starred2", subscriptions_url: "subscriptions2", organizations_url: "organizations2", repos_url: "repos2", events_url: "events2", received_events_url: "received_events2", type: "type2", site_admin: true)
+            GHEResponse(login: "user1", id: 1, node_id: "node1", avatar_url: "avatar1", gravatar_id: "gravatar1", url: "url1", html_url: "html1", followers_url: "followers1", following_url: "following1", gists_url: "gists1", starred_url: "starred1", subscriptions_url: "subscriptions1", organizations_url: "organizations1", repos_url: "repos1", events_url: "events1", received_events_url: "received_events1", type: "type1", site_admin: false, name: "name", blog: "blog"),
+            GHEResponse(login: "user2", id: 2, node_id: "node2", avatar_url: "avatar2", gravatar_id: "gravatar2", url: "url2", html_url: "html2", followers_url: "followers2", following_url: "following2", gists_url: "gists2", starred_url: "starred2", subscriptions_url: "subscriptions2", organizations_url: "organizations2", repos_url: "repos2", events_url: "events2", received_events_url: "received_events2", type: "type2", site_admin: true, name: "name", blog: "blog")
         ]
     }
 }
