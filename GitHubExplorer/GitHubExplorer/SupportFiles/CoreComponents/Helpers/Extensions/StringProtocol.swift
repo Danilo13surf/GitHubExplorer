8b95eed3ada1381fe90db6247b829422909fc978
Danilo Carlos Ribeiro <d.c.ribeiroo@gmail.com>
2023-06-25 12:10:18 -0300

    add new helper StringProtocol

---
+++ GitHubExplorer/GitHubExplorer/SupportFiles/CoreComponents/Helpers/Extensions/StringProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  GitHubExplorer
+//
+//  Created by Danilo Carlos Ribeiro on 25/06/23.
+//  Copyright © 2023 GitHubExplorer. All rights reserved.
+//
+
+import Foundation
+
+public extension StringProtocol {
+    var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
+    var firstLowercased: String { prefix(1).lowercased() + dropFirst() }
+    var firstCapitalized: String { prefix(1).capitalized + dropFirst() }
+}
